beceb952be39425a042af6b5389dd1c865987be9
Radu <radu.stanciu2016@gmail.com>
2024-05-27 19:03:04 +0300

    made basic operation functions

---
+++ script.js
@@ -0,0 +1,37 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;  
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if(b != 0){
+    return a / b;
+  }
+  else {
+    return "cannot divide by zero";
+  };
+}
+
+function operate(a, operator, b) {
+  switch(operator) {
+    case "+":
+      return add(a, b);
+      break;
+    case: "-":
+      return subtract(a, b);
+      break;
+    case: "*":
+      return multiply(a, b);
+      break;
+    case: "/":
+      return divide(a, b);
+      break;
+  }
+}
