b30c72e747525fe492f0966df76c7c384f3555ef
Radu <radu.stanciu2016@gmail.com>
2024-05-29 10:50:04 +0300

    made almost bugproof typing rules

--- script.js
+++ script.js
@@ -36,22 +36,25 @@ function operate(a, operator, b) {
   }
 }
 
-displayValue = document.querySelector("#calculator-screen");
+const displayValue = document.querySelector("#calculator-screen");
 
 buttonsNodelist = document.querySelectorAll(".calculator-box button");
 
-buttons = {};
+let buttons = {};
 for (let i = 0; i < buttonsNodelist.length; i++){
   buttons[buttonsNodelist[i].textContent] = buttonsNodelist[i];
   if (["=","Clear","Delete"].includes(buttonsNodelist[i].textContent) == false) {
-    if(["/","x","+","-","."].includes(buttonsNodelist[i].textContent)){
+    operatorBtns = ["/","x","+","-","."];
+    if(operatorBtns.includes(buttonsNodelist[i].textContent)){
       buttons[buttonsNodelist[i].textContent].onclick = (e) => {
         console.log(displayValue.textContent.slice(-1), e.target.textContent)
-        if(displayValue.textContent.slice(-1) != e.target.textContent){
-          displayValue.textContent += e.target.textContent;
-        }
-        else {
-          displayValue.textContent.replace(/.$/, e.target.textContent);
+        if(!(displayValue.textContent.slice(-1) == "-" && e.target.textContent == "-")){
+          if(operatorBtns.includes(displayValue.textContent.slice(-1)) && (e.target.textContent != "-" /*&& displayValue.textContent.slice(-1) == "-"*/)){ // these combos should work: /-, --, *-
+            displayValue.textContent = displayValue.textContent.replace(/.$/, e.target.textContent);
+          }
+          else {
+            displayValue.textContent += e.target.textContent;
+          }
         }
       }
     }
