28cb3ac89c645d0a88e12a920ba70ce47d4c03ff
Radu <radu.stanciu2016@gmail.com>
2024-06-01 15:13:05 +0300

    fixed negative number bugs
    -4-11 would result in an infinite loop and crash the site
    -45/-5 also didn't work

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ for (let i = 0; i < buttonsNodelist.length; i++){
         let btnValue = e.target.textContent;
 
         //add new character if last character is a number OR last character is '-' and: last two characters aren't an operator and a '-', last character isn't a '.', displayValue isn't empty
-        if((lastChar >= '0' && lastChar <= '9') || (btnValue == "-" && (operatorBtns.includes(secondLastChar) && lastChar == '-') == false && ['','.'].includes(lastChar) == false)){
+        if((lastChar >= '0' && lastChar <= '9') || (btnValue == "-" && (operatorBtns.includes(secondLastChar) && lastChar == '-') == false && ['.'].includes(lastChar) == false)){
           displayValue.textContent += btnValue;
         }
         //replace last character if displayValue isn't empty and last two characters aren't an operator and a '-'
@@ -109,20 +109,28 @@ let opOrder = [/[*/]/, /[+-]/];
 
 buttons["="].onclick = () => {
   for(let i = 0; i < opOrder.length; i++) {
-    while(displayValue.textContent.search(opOrder[i]) != -1){
-      let operatorIndex = displayValue.textContent.search(opOrder[i]);
+    while(displayValue.textContent.slice(1).search(opOrder[i]) != -1){
+      if(displayValue.textContent[0] == '-'){
+        operatorIndex = displayValue.textContent.slice(1).search(opOrder[i]) + 1;
+      }
+      else {
+        operatorIndex = displayValue.textContent.search(opOrder[i]);
+      }
 
       let startIndex = 0;
-      for(let i = operatorIndex - 1; /[*/+-]/.test(displayValue.textContent[i]) == false && i >= 0; i--){
+      for(let i = operatorIndex - 1; (/[*/+-]/.test(displayValue.textContent[i]) == false && i >= 0) || (displayValue.textContent[i] == '-' && i == 0); i--){
         startIndex = i;
       }
 
       let endIndex = 0;
-      for(let i = operatorIndex + 1; /[*/+-]/.test(displayValue.textContent[i]) == false && i < displayValue.textContent.length; i++){
+      for(let i = operatorIndex + 1; (/[*/+-]/.test(displayValue.textContent[i]) == false && i < displayValue.textContent.length) || (displayValue.textContent[i] == '-' && i == operatorIndex+1); i++){
         endIndex = i;
       }
 
       let result = operate(Number(displayValue.textContent.substring(startIndex,operatorIndex)), displayValue.textContent[operatorIndex], Number(displayValue.textContent.substring(operatorIndex+1, endIndex + 1)));
+      if(result % 1 != 0){
+        result = result.toFixed(4);
+      }
       displayValue.textContent = displayValue.textContent.substring(0, startIndex) + result + displayValue.textContent.substring(endIndex+1, displayValue.textContent.length)
     }
   }
